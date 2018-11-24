ea0384dba5def588161422ed1e3a24ba16bfeca1
gaoyunfeng <gaoyunfeng@lanxum.com>
2018-11-24 12:50:24 +0800

    解释器模式

---
+++ src/main/java/com/yunus/behavior/interpreter/Expression.java
@@ -0,0 +1,11 @@
+package com.yunus.behavior.interpreter;
+
+// Expression
+public abstract class Expression {
+	
+	public abstract int interpret();
+	
+	@Override
+	public abstract String toString();
+
+}
